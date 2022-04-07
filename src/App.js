b9064397b998af1e682332e7713570c386deab43
Eder Duran <ederduran@Eders-MacBook-Pro.local>
2022-04-07 15:59:19 -0500

    merge changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
 import Home from "./components/home/Home";
 import { useState } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch, Router } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Footer from "./components/footer/Footer";
 import RentCar from "./components/rent-car/RentCar";
-import LoginForm from "./components/logIn/LoginForm";
+import Profile from "./components/profile/Profile";
+import PrivateRoute from "./components/PrivateRoute";
+import history from "./utils/history";
+
 function App(props) {
-  const [activeRent, setActiveRent] = useState(true);
+	const [activeRent, setActiveRent] = useState(true);
 
-  const rentHandler = () => {
-    console.log("clicked");
-    setActiveRent(null);
-  };
+	const rentHandler = () => {
+		console.log("clicked");
+		setActiveRent(null);
+	};
 
-  return (
-    <div>
-      <Route path="/" exact>
-        <Home />
-        {/* <LoginForm /> */}
-      </Route>
-      <Route path="/rent-car">
-        <Layout>
-          <RentCar />
-        </Layout>
-        <Footer />
-      </Route>
-    </div>
-    // {activeRent && <Home onShowRent={rentHandler} />};
-  );
+	return (
+		<div>
+			<Router history={history}>
+				<Switch>
+					<Route path="/" exact>
+						<Home />
+						{/* <LoginForm /> */}
+					</Route>
+					<PrivateRoute path="/rent-car">
+						<Layout>
+							<RentCar />
+						</Layout>
+						<Footer />
+					</PrivateRoute>
+					<PrivateRoute path="/profile">
+						<Layout>
+							<Profile />
+						</Layout>
+						<Footer />
+					</PrivateRoute>
+				</Switch>
+			</Router>
+		</div>
+		// {activeRent && <Home onShowRent={rentHandler} />};
+	);
 }
 
 export default App;
-// https://github.com/cluedevs/ESL_back/blob/main/api/api-gateway-openapi.yaml
-// https://editor.swagger.io/
--- src/index.js
+++ src/index.js
@@ -3,13 +3,35 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import CarContextProvider from "./context/Carcontext";
 import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "./context/react-auth0-spa";
+import config from "./auth_config.json";
+import history from "./utils/history";
+
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = (appState) => {
+	history.push(
+		appState && appState.targetUrl
+			? appState.targetUrl
+			: window.location.pathname
+	);
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <CarContextProvider>
-        <App />
-      </CarContextProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<BrowserRouter>
+			<Auth0Provider
+				domain={config.domain}
+				client_id={config.clientId}
+				redirect_uri={window.location.origin}
+				audience={config.audience}
+				onRedirectCallback={onRedirectCallback}
+			>
+				<CarContextProvider>
+					<App />
+				</CarContextProvider>
+			</Auth0Provider>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
